6995050933469ce12c084b83ff5112c16d9c26da
0zz1cl13 <seniorcl13@gmail.com>
2019-12-26 16:38:23 -0400

    Config banco

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/nodeapi', { 
+  useUnifiedTopology: true, 
+  useNewUrlParser: true
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.send('Heh !');
 });
 
 app.listen(3001);
