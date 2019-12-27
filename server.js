dea247d8ca41286c43798e9cbba29347a7e0324b
0zz1cl13 <seniorcl13@gmail.com>
2019-12-26 22:22:35 -0400

    organizacao de arquivos do projeto - controllers, models e routes

--- server.js
+++ server.js
@@ -10,10 +10,9 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {
   useNewUrlParser: true
 });
 
+
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
